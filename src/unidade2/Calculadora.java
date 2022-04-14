761464784b0f1688df816cb1be0254620bdb113c
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-04-14 16:06:03 -0300

    Exercicios static

---
+++ src/unidade2/Calculadora.java
@@ -0,0 +1,7 @@
+package unidade2;
+
+// Escreva uma classe para representar uma calculadora que realiza as quatro operações básicas. Os métodos dessa classe devem aceitar qualquer combinação de tipos números como seu parâmetros (e.g.: soma(5,3.2f), divide(5.1,17)...). Não utilize sobrecarga de métodos. Escreva um aplicativo de teste que demonstra as capacidades da classe criada.
+
+public class Calculadora {
+
+}
---
+++ src/unidade2/CalculadoraTeste.java
@@ -0,0 +1,7 @@
+package unidade2;
+
+public class CalculadoraTeste {
+  public static void main(String[] args) {
+   
+  }
+}
