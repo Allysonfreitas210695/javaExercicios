cfd7abfe2c380549e58bf86969c80f3c089ae48b
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-04-14 19:36:48 -0300

    reajuste feito

--- src/unidade2/Calculadora.java
+++ src/unidade2/Calculadora.java
@@ -3,5 +3,64 @@ package unidade2;
 // Escreva uma classe para representar uma calculadora que realiza as quatro operações básicas. Os métodos dessa classe devem aceitar qualquer combinação de tipos números como seu parâmetros (e.g.: soma(5,3.2f), divide(5.1,17)...). Não utilize sobrecarga de métodos. Escreva um aplicativo de teste que demonstra as capacidades da classe criada.
 
 public class Calculadora {
+  
+  public static double soma(Object n1, Object n2){
+    if((n1 instanceof Integer) && ((n2 instanceof Integer))){
+      return ((Integer) n1 + (Integer) n2);
+    }else if((n1 instanceof Integer) && (n2 instanceof Double)){
+      return ((Integer) n1 + (Double) n2);
+    }else if((n1 instanceof Double) && (n2 instanceof Integer)){
+      return ((Double) n1 + (Integer) n2);
+    }else if ((n1 instanceof Double) && (n2 instanceof Double)){
+      return ((Double) n1 + (Double) n2);
+    }else{
+      System.err.println("Variavel não permitida");
+      return 0;
+    }
+  }
 
+  public static double subtracao(Object n1, Object n2){
+    if((n1 instanceof Integer) && ((n2 instanceof Integer))){
+      return ((Integer) n1 - (Integer) n2);
+    }else if((n1 instanceof Integer) && (n2 instanceof Double)){
+      return ((Integer) n1 - (Double) n2);
+    }else if((n1 instanceof Double) && (n2 instanceof Integer)){
+      return ((Double) n1 - (Integer) n2);
+    }else if ((n1 instanceof Double) && (n2 instanceof Double)){
+      return ((Double) n1 - (Double) n2);
+    }else{
+      System.err.println("Variavel não permitida");
+      return 0;
+    }
+  }
+
+  public static double divisao(Object n1, Object n2){
+    if((n1 instanceof Integer) && ((n2 instanceof Integer))){
+      return ((Integer) n1 / (Integer) n2);
+    }else if((n1 instanceof Integer) && (n2 instanceof Double)){
+      return ((Integer) n1 / (Double) n2);
+    }else if((n1 instanceof Double) && (n2 instanceof Integer)){
+      return ((Double) n1 / (Integer) n2);
+    }else if ((n1 instanceof Double) && (n2 instanceof Double)){
+      return ((Double) n1 / (Double) n2);
+    }else{
+      System.err.println("Variavel não permitida");
+      return 0;
+    }
+  }
+
+  public static double multiplicao(Object n1, Object n2){
+    if((n1 instanceof Integer) && ((n2 instanceof Integer))){
+      return ((Integer) n1 * (Integer) n2);
+    }else if((n1 instanceof Integer) && (n2 instanceof Double)){
+      return ((Integer) n1 * (Double) n2);
+    }else if((n1 instanceof Double) && (n2 instanceof Integer)){
+      return ((Double) n1 * (Integer) n2);
+    }else if ((n1 instanceof Double) && (n2 instanceof Double)){
+      return ((Double) n1 * (Double) n2);
+    }else{
+      System.err.println("Variavel não permitida");
+      return 0;
+    }
+  }
 }
--- src/unidade2/CalculadoraTeste.java
+++ src/unidade2/CalculadoraTeste.java
@@ -2,6 +2,9 @@ package unidade2;
 
 public class CalculadoraTeste {
   public static void main(String[] args) {
-   
+   System.out.println(Calculadora.soma(1, 2.4));
+   System.out.println(Calculadora.subtracao(1, 2.4));
+   System.out.println(Calculadora.divisao(2,2));
+   System.out.println(Calculadora.multiplicao(1, 10));
   }
 }
